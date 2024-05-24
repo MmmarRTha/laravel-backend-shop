390a6318b69fdc2837e3571f81c86a22110d156d
MmmarRTHa <m27842n@outlook.com>
2024-05-24 14:23:47 -0700

    - update feature/products_category - add API Resources for categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoryCollection;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return response()->json(['categories' => Category::all()]);
+        //return response()->json(['categories' => Category::all()]);
+        return new CategoryCollection(Category::all());
     }
 }
