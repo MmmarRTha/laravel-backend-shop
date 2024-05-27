a179669dcc05ef1df0c40dbcbf301b2c1267a5e3
MmmarRTHa <m27842n@outlook.com>
2024-05-26 20:28:16 -0700

    - update: feature/products_endpoint - add API resources for products

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/user', function (Request $request) {
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/products', ProductController::class);
