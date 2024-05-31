1bd88c866d063439e72bbc8194e78900d4fa83de
MmmarRTHa <m27842n@outlook.com>
2024-05-30 17:06:46 -0700

    - update: feature/authentication - add user authentication

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/products', ProductController::class);
