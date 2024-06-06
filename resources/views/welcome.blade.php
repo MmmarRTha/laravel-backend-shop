17d022633fcff2e6e4f242dd644ba34a1e6722cd
MmmarRTHa <m27842n@outlook.com>
2024-06-05 22:51:50 -0700

    - wip: feature/admin_dashboard - working on it

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1 class="mt-10 text-4xl font-bold text-center">Welcome to a Laravel and React App</h1>
+</body>
+</body>
+</html>
