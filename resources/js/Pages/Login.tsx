15062b3fc56a1bbee06eb70fc67d3556a97c09a1
MmmarRTHa <m27842n@outlook.com>
2025-07-06 23:45:13 -0700

    - update: feature/inertia - add inertia and react

---
+++ resources/js/Pages/Login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+   return (
+    <>
+        <h1 className="text-4xl font-black">Sign In</h1>
+        <p>To Order now you must sing in</p>
+    </>
+  )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel(['resources/js/app.tsx']),
+        react(),
+        tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
 });
